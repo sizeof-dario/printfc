56736d400b2d95525db7cd0e9a2efd412f1aa244
sizeof-dario <dimeo.dario.h@gmail.com>
2025-11-25 22:03:25 +0100

    The library can be built and linked successfully

---
+++ include/printfc.h
@@ -0,0 +1,217 @@
+
+/*************************************************************************//***
+ * @file printfc.h
+ * 
+ * @brief Include this header file to use the `printfc` function.
+ * 
+ ******************************************************************************/
+
+#ifndef PRINTFC_H
+#define PRINTFC_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+/** @brief Single byte (0-255). */
+typedef unsigned char byte;
+
+
+
+/** 
+ * @brief Double word (4 bytes).
+ * 
+ * 4-byte size guaranteed via static assertion.
+*/
+typedef unsigned int dword;
+
+
+
+// Guarantees dword's size is 4 bytes.
+_Static_assert(sizeof(dword) == 4, "dword must be 4 bytes.");
+
+
+
+/**
+ * @brief Size of a `color_t` storing a 24-bit color escape sequence.
+ * 
+ * A value of 20 is chosen to accommodate for the maximum length such sequences
+ * can reach, including the null-terminator.
+ * 
+ */
+#define COLOR_T_LENGTH 20
+
+
+
+/**
+ * @brief Represents an escape sequence used to change colors in terminals that
+ *      support 24-bit colors.
+ * 
+ * Such sequences take the form "E[L8;2;R;G;Bm", where
+ * 
+ *      - E is the escape character.
+ * 
+ *      - L is either 3 (for foreground) or 4 (for background).
+ * 
+ *      - R, G and B are respectively the red, green and blue components of the
+ *        represented color.
+ * 
+ * The sequence is stored as a null-terminated string of length `COLOR_T_LENGTH`.
+ * 
+ */
+typedef char color_t[COLOR_T_LENGTH];
+
+
+
+/** 
+ * @brief Template string used by the `tocolor` function to build a color
+ *      sequence based on the provided arguments.
+ * 
+ * The initial null-terminator makes the literal unusable as a regular C string,
+ * ensuring that it is accessed only through explicit manipulation.
+ * 
+ */
+#define COLOR_T_TEMPLATE "\0[08;2;000;000;000m"
+
+
+
+/**
+ * @brief Copies source `color_t` to destination `color_t`.
+ * 
+ * @param destination A pointer to the `color_t` to copy to.
+ * @param source A pointer to the `color_t` to copy from.
+ * @return A pointer to destination after the copy.
+ * 
+ * @note Since `color_t` is defined to be a 20-characters-long array, this
+ *      function really works as a null-terminator-ignoring `strcpy` over
+ *      20-characters-long C strings. Thus, it can be used to work with
+ *      `COLOR_T_TEMPLATE`.
+ */
+static inline color_t *clrcpy (
+    color_t *const restrict destination,
+    const color_t *const restrict source
+)   
+{
+    for (byte i = 0; i < COLOR_T_LENGTH; i++)
+    {
+        (*destination)[i] = (*source)[i];
+    }
+}
+
+
+
+/**
+ * @brief Specifies the terminal layer for functions using `color_t`.
+ *
+ * This type is used to indicate whether a color should be applied to the
+ * foreground or the background.
+ * 
+ */
+typedef enum layer { 
+    FOREGROUND = '3', /**< Foreground layer. */
+    BACKGROUND = '4'  /**< Background layer. */
+} layer_t;
+
+
+
+/** Extracts the red component from a 24-bit RGB color value. */
+#define RGB_GET_R(x) (byte)(x >> 16)
+
+
+
+/** Extracts the green component from a 24-bit RGB color value. */
+#define RGB_GET_G(x) (byte)(x >> 8)
+
+
+
+/** Extracts the blue component from a 24-bit RGB color value. */
+#define RGB_GET_B(x) (byte)(x)
+
+
+
+/**
+ * @brief Converts a 24-bit RGB color value into a `color_t` type.
+ * 
+ * @param color_p A pointer to a `color_t` to store the conversion result.
+ * @param value The 24-bit RGB value of the color to convert.
+ * @param layer The terminal layer of the color to convert, either being
+ *      `FOREGROUND` or `BACKGROUND`.
+ * 
+ */
+void tocolor (
+    color_t *const restrict color_p, 
+    const int value, 
+    const layer_t layer
+);
+
+
+
+/** 
+ * @brief Escape sequence used to restore both foreground and background colors
+ *      to their default values.
+ * 
+ */
+#define DEFAULT_COLORS_SEQUENCE "\e[39m\e[49m"
+
+
+
+/** @brief Lenght of the `DEFAULT_COLORS_SEQUENCE` string. */
+#define DEFAULT_COLORS_SEQUENCE_LENGTH 11
+
+
+
+/**
+ * @brief Writes formatted and colorized output to stdout.
+ *      
+ * @param format A format string that may contain both printf-like format
+ *      specifiers (%) and color specifiers (#).
+ *
+ *      - Format specifiers behave like those of printf, except that the
+ *        character '#' is reserved. To obtain the literal behaviour used by
+ *        printf, each '#' must be doubled as '##'.
+ *
+ *      - Color specifiers indicate foreground or background color changes: a #f
+ *        or #F allows to change the foreground color, while a #b or #B is used
+ *        for background.
+ *        A literal '#' character is written using '##', analogous to how printf
+ *        uses '%%' for a literal '%'.
+ *
+ * @param ... If the format string contains any specifiers, the function expects
+ *      additional arguments. All color values – as RGB values – must appear
+ *      first, in the same order as their corresponding color specifiers in the
+ *      format string. After these, the remaining arguments correspond to the
+ *      printf-like format specifiers, following the same rules as printf.
+ *
+ * @return The number of characters written to stdout, excluding any color
+ *      control sequences.
+ *
+ * @note Color sequences do not count toward the return value.
+ * 
+ * @warning Passing arguments in the wrong order results in undefined behaviour.
+ * 
+ */
+int printfc(const char *format, ...);
+
+
+
+/**
+ * @brief Converts independent red, green and blue components to a whole RGB
+ *      color value.
+ *  
+ * @param r Red component (0-255).
+ * @param g Green component (0-255).
+ * @param b Blue component (0-255).
+ * 
+ * @return The color of RGB components r, g and b as a 24-bit unsigned int.
+ * 
+ */
+#define RGB(r, g, b) \
+    (((dword)(byte)(r) << 16) | ((dword)(byte)(g) << 8) | (dword)(byte)(b))
+
+
+
+#endif // PRINTFC_H
+
---
+++ printfc.c
@@ -0,0 +1,126 @@
+#include "printfc.h"
+
+
+
+void tocolor (
+    color_t *const restrict color_p, 
+    const int value,
+    const layer_t layer
+)   
+{
+    // Prepares color_p.
+    clrcpy(color_p, &COLOR_T_TEMPLATE);
+    (*color_p)[0] = '\e'; 
+    (*color_p)[2] = layer;
+
+    // Extracts value components.
+    byte r = RGB_GET_R(value);
+    byte g = RGB_GET_G(value);
+    byte b = RGB_GET_B(value);
+
+    // Converts value components into characters and completes color_p.
+    for (byte i = 0; i < 3; i++)
+    {
+        // Converts first decimal digit to character, then divides by ten to
+        // shift digits towards the right. 
+        (*color_p)[9 - i] = r % 10 + '0';
+        r /= 10;
+
+        (*color_p)[13 - i] = g % 10 + '0';
+        g /= 10;
+
+        (*color_p)[17 - i] = b % 10 + '0';
+        b /= 10;
+    }
+}
+
+int printfc(const char* format, ...)
+{   
+    /** vvv * Calculates bufsize **********************************************/
+
+    int hashcount = 0;
+    for (int i = 0; format[i]; i++)
+    {
+        if (format[i] == '#')
+        {
+            hashcount++;
+        }
+    }
+    
+    int bufsize = strlen(format) 
+        + COLOR_T_LENGTH * hashcount 
+        + DEFAULT_COLORS_SEQUENCE_LENGTH;
+
+    /** ^^^ * Calculates bufsize **********************************************/
+
+    // Gets the buffer.
+    char *buffer = (char*)malloc(bufsize);
+    if(!buffer) return -1;
+
+    /** vvv * Color formatting ************************************************/
+
+    int bufidx = 0; // Index iterating over the buffer.
+    int ftmidx = 0; // Index iterating over the format string.
+    int written = 0;
+    color_t temp_color;
+    va_list ap;
+    va_start(ap, format);
+    while(bufidx < bufsize && format[ftmidx])
+    {
+        if (format[ftmidx] == '#')
+        {
+            switch (format[ftmidx + 1])
+            {
+            case 'f':
+            case 'F':
+                tocolor(&temp_color, va_arg(ap, int), FOREGROUND);
+                strcpy(buffer + bufidx, temp_color);
+                bufidx += COLOR_T_LENGTH - 1; // Excluding the null-terminator.
+                ftmidx += 2;
+                written -= COLOR_T_LENGTH - 1;
+                break;
+            
+            case 'b':
+            case 'B':
+                tocolor(&temp_color, va_arg(ap, int), BACKGROUND);
+                strcpy(buffer + bufidx, temp_color);
+                bufidx += COLOR_T_LENGTH - 1;
+                ftmidx += 2;
+                written -= COLOR_T_LENGTH - 1;
+                break;
+
+            case '#':
+                buffer[bufidx] = '#';
+                bufidx++;
+                ftmidx += 2;
+                break;
+
+            default:
+                buffer[bufidx] = format[ftmidx];
+                bufidx++;
+                ftmidx++;
+                break;
+            }
+        }
+        else
+        {
+            buffer[bufidx] = format[ftmidx];
+            bufidx++;
+            ftmidx++;
+        }
+    }
+
+    strcpy(buffer + bufidx, DEFAULT_COLORS_SEQUENCE);
+
+    /** ^^^ * Color formatting ************************************************/
+
+    // Calls vprintf and updates written. The +1 is needed to exclude the final
+    // null-terminator that is othewise counted with 
+    // `DEFAULT_COLORS_SEQUENCE_LENGTH`.
+    written += vprintf(buffer, ap) - DEFAULT_COLORS_SEQUENCE_LENGTH + 1;
+
+    // Cleans and return
+    va_end(ap);
+    free(buffer);
+    return written;
+}
