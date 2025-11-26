cf5a64ec6161bf155a478f9caa1abf710c1de116
sizeof-dario <dimeo.dario.h@gmail.com>
2025-11-26 20:07:32 +0100

    bash file

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,3 +6,5 @@ git clone --depth 1 https://github.com/sizeof-dario/printfc.git
 cd ./printfc/
 cmake -B build 
 cmake --build build
+mv ./printfc/build/libprintfc.a ./
+find . ! -name 'libprintfc.a' -exec rm -r {} +
