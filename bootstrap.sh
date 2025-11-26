eedfd39ee8fb830839c50ad20cf4a523a86eefe9
malloc <dimeo.dario.h@gmail.com>
2025-11-26 20:19:19 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,5 @@ git clone --depth 1 https://github.com/sizeof-dario/printfc.git
 cd ./printfc/
 cmake -B build 
 cmake --build build
-ls
-mv ./printfc/build/libprintfc.a ./
+mv ./build/libprintfc.a ./
 find . ! -name 'libprintfc.a' -exec rm -rf {} +
